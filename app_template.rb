687ac260d945b6acb94897da3826a9a44f55d2e4
shu0115 <s.matsumoto0115@gmail.com>
2018-01-12 18:45:49 +0900

    Update

--- app_template.rb
+++ app_template.rb
@@ -56,19 +56,19 @@ end
 # create_file "app/assets/javascripts/application.js.rb", content.force_encoding('ASCII-8BIT')
 
 # ---------- Config ---------- #
-# Procfile
+## Procfile
 content = "web: bundle exec puma -C config/puma.rb\n"
 create_file 'Procfile', content.force_encoding('ASCII-8BIT')
 
-# puma.rb
+## puma.rb
 # content = Net::HTTP.get URI.parse('https://raw.githubusercontent.com/shu0115/rails4_app_template/master/templates/config/puma.rb')
 # create_file 'config/puma.rb', content.force_encoding('ASCII-8BIT')
 
-# database_connection.rb
+## database_connection.rb
 # content = Net::HTTP.get URI.parse('https://raw.githubusercontent.com/shu0115/rails4_app_template/master/templates/config/initializers/database_connection.rb')
 # create_file 'config/initializers/database_connection.rb', content.force_encoding('ASCII-8BIT')
 
-# application.rb setting
+## application.rb
 #insert_into_file "config/application.rb", "    config.i18n.default_locale     = :ja\n    I18n.enforce_available_locales = false\n", after: "# -- all .rb files in that directory are automatically loaded.\n"
 content = "    config.time_zone = 'Tokyo'\n"
 insert_into_file "config/application.rb", content.force_encoding('ASCII-8BIT'), after: "# -- all .rb files in that directory are automatically loaded.\n"
@@ -81,20 +81,29 @@ insert_into_file "config/application.rb", content.force_encoding('ASCII-8BIT'), 
 # content += "\n"
 # insert_into_file "config/application.rb", content.force_encoding('ASCII-8BIT'), after: "class Application < Rails::Application\n"
 
-# production.rb setting
+## production.rb
 insert_into_file "config/environments/production.rb", "  config.force_ssl = true\n", after: "# config.force_ssl = true\n"  # 強制SSL設定
 
-# # development.rb setting
+# ## development.rb
 # gsub_file "config/environments/development.rb", /(config.assets.debug = true)+/, "# config.assets.debug = true"                  # コメントアウト追加
 # insert_into_file "config/environments/development.rb", "  config.assets.debug = false\n", after: "config.assets.debug = true\n"  # false設定追加
+content =  "\n"
+content += "  # メーラ設定\n"
+content += "  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }\n"
+content += "  config.action_mailer.delivery_method     = :letter_opener\n"
+insert_into_file "config/environments/development.rb", content.force_encoding('ASCII-8BIT'), after: "config.file_watcher = ActiveSupport::EventedFileUpdateChecker\n"
 
-# Add Strong Parameters config
+## Add Strong Parameters config
 insert_into_file "config/environments/development.rb",
   "\n  # Strong Parametersエラー例外発生\n  config.action_controller.action_on_unpermitted_parameters = :raise\n",
   after: "# config.action_view.raise_on_missing_translations = true\n"
 
-# # rails-flog setting
+# ## rails-flog
 # create_file 'tmp/no-flog-sql.txt'
+
+## routes.rb
+insert_into_file "config/routes.rb", "\n  mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?\n", after: "  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html\n"
+
 
 # ---------- Gemfile ---------- #
 # # Ruby Version Insert
